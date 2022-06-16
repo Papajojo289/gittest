44361c465ca2aaf6872bb9949ef3f8b13e4b27ee
Papajojo289 <yoyospitzvygotsky@hotmail.com>
2022-06-16 14:43:36 +0100

    These files are to commit to github

---
+++ planet.java
@@ -0,0 +1,9 @@
+class Planet {
+    void revolve() {
+        System.out.println("This planet is revolving!");
+    }
+    public static void main(String[] args) {
+        Planet earth = new Planet(); 
+        earth.revolve();
+    }
+}
